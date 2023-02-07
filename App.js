ebe627f3382fe10027813238cd018bfc07b0b44e
Your Name <you@example.com>
2023-02-07 13:11:05 -0300

    testing

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hola!</Text>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+  <View style={styles.header}>
+  <TouchableOpacity>
+  <Ionicons name="ios-menu" size={32} color="#000" />
+  </TouchableOpacity>
+  <Text style={styles.headerTitle}>Menú</Text>
+  </View>
+  <Text style={styles.title}>Economia del Conocimiento de Córdoba</Text>
+  <Image
+  source={require('./assets/landing.png')}
+  style={styles.image}
+  />
+  <StatusBar style="auto" />
+  </View>
   );
-}
-
-const styles = StyleSheet.create({
+  }
+  
+  const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flex: 1,
+  backgroundColor: '#fff',
+  alignItems: 'center',
+  justifyContent: 'center',
   },
-});
+  header: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  paddingTop: 40,
+  paddingHorizontal: 20,
+  },
+  headerTitle: {
+  fontWeight: 'bold',
+  fontSize: 20,
+  },
+  image: {
+  width: 400,
+  height: 200,
+  marginTop: -500,
+  },
+  title: {
+  fontWeight: 'bold',
+  fontSize: 20,
+  marginTop: -50,
+  },
+  });
